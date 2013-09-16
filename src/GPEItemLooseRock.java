e971f11689fcdeba254e256818a216752ca0f9f8
Grom PE <i@grompe.org.ru>
2013-09-16 22:06:40 +0700

    Convert loose rocks to cobblestone blocks by shift-right-clicking

--- src/GPEItemLooseRock.java
+++ src/GPEItemLooseRock.java
@@ -12,6 +12,14 @@ public class GPEItemLooseRock extends Item
 
   public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
   {
+    if (player.isSneaking())
+    {
+      if (!consumeInventoryRocks(player.inventory)) return stack;
+      world.playSoundAtEntity(player, "dig.stone", 0.5F, 0.3F + itemRand.nextFloat() * 0.1F);
+      ItemStack cobble = new ItemStack(Block.cobblestone, 1, 1);
+      if (!player.inventory.addItemStackToInventory(cobble)) player.dropPlayerItem(cobble);
+      return stack;
+    }
     if (!player.capabilities.isCreativeMode)
     {
       if (player.getFoodStats().getFoodLevel() <= 18)
@@ -28,5 +36,36 @@ public class GPEItemLooseRock extends Item
     world.playSoundAtEntity(player, "random.bow", 0.5F, 0.3F + itemRand.nextFloat() * 0.1F);
     if (!world.isRemote) world.spawnEntityInWorld(new GPEEntityRock(world, player));
     return stack;
+  }
+
+  boolean consumeInventoryRocks(IInventory inventory)
+  {
+    int i, count = 0, max = inventory.getSizeInventory();
+    ItemStack stack;
+    for (i = max - 1; i >= 0; i--)
+    {
+      stack = inventory.getStackInSlot(i);
+      if (stack != null && stack.getItem().itemID == itemID) count += stack.stackSize;
+      if (count >= 4) break;
+    }
+    if (count < 4) return false;
+    count = 4;
+    for (i = max - 1; i >= 0; i--)
+    {
+      stack = inventory.getStackInSlot(i);
+      if(stack != null && stack.getItem().itemID == itemID)
+      {
+        if (stack.stackSize > count)
+        {
+          stack.stackSize -= count;
+          //inventory.setInventorySlotContents(i, stack);
+          return true;
+        }
+        count -= stack.stackSize;
+        inventory.setInventorySlotContents(i, (ItemStack)null);
+        if (count == 0) break;
+      }
+    }
+    return true;
   }
 }
