84e8f5efbeccedc3c53b231e675da95fdc9f3f53
Grom PE <i@grompe.org.ru>
2014-05-22 11:08:45 +0200

    Allow resting in beds again; without any other effects for now

--- patcher/scripts/tweaks.js
+++ patcher/scripts/tweaks.js
@@ -942,6 +942,13 @@ function ObjectArray(arr)
       {
       },
     },*/
+    "FCBlockBed":
+    {
+      removeMethods:
+      [
+        "a(Laab;IIILsq;IFFF)Z"
+      ],
+    },
     "FCBlockBlockDispenser":
     {
       tweakMethods:
@@ -2223,6 +2230,254 @@ function ObjectArray(arr)
             ],
             "\t* Making a player slower with armor and faster with boots in "
           ).process(mn);
+        },
+        "a(III)Lsr;": function(mn)
+        {
+          check(mn, 0x2160163);
+          var l0 = LabelNode(),
+              l1 = LabelNode(),
+              l2 = LabelNode(),
+              l3 = LabelNode(),
+              l4 = LabelNode(),
+              l5 = LabelNode(),
+              l6 = LabelNode(),
+              l7 = LabelNode(),
+              l8 = LabelNode(),
+              l9 = LabelNode(),
+              l10 = LabelNode();
+          CodeInserter(
+            BeginningFinder(),
+            [
+              VarInsnNode(ALOAD, 0),
+              FieldInsnNode(GETFIELD, "sq", "q", "Laab;"),
+              FieldInsnNode(GETFIELD, "aab", "I", "Z"),
+              JumpInsnNode(IFNE, l0),
+              VarInsnNode(ALOAD, 0),
+              MethodInsnNode(INVOKEVIRTUAL, "sq", "bz", "()Z"),
+              JumpInsnNode(IFNE, l1),
+              VarInsnNode(ALOAD, 0),
+              MethodInsnNode(INVOKEVIRTUAL, "sq", "R", "()Z"),
+              JumpInsnNode(IFNE, l2),
+              l1,
+              FrameNode(F_SAME, 0, null, 0, null),
+              FieldInsnNode(GETSTATIC, "sr", "e", "Lsr;"),
+              InsnNode(ARETURN),
+              l2,
+              FrameNode(F_SAME, 0, null, 0, null),
+              VarInsnNode(ALOAD, 0),
+              FieldInsnNode(GETFIELD, "sq", "u", "D"),
+              VarInsnNode(ILOAD, 1),
+              InsnNode(I2D),
+              InsnNode(DSUB),
+              MethodInsnNode(INVOKESTATIC, "java/lang/Math", "abs", "(D)D"),
+              LdcInsnNode(Double("3.0")),
+              InsnNode(DCMPL),
+              JumpInsnNode(IFGT, l3),
+              VarInsnNode(ALOAD, 0),
+              FieldInsnNode(GETFIELD, "sq", "v", "D"),
+              VarInsnNode(ILOAD, 2),
+              InsnNode(I2D),
+              InsnNode(DSUB),
+              MethodInsnNode(INVOKESTATIC, "java/lang/Math", "abs", "(D)D"),
+              LdcInsnNode(Double("2.0")),
+              InsnNode(DCMPL),
+              JumpInsnNode(IFGT, l3),
+              VarInsnNode(ALOAD, 0),
+              FieldInsnNode(GETFIELD, "sq", "w", "D"),
+              VarInsnNode(ILOAD, 3),
+              InsnNode(I2D),
+              InsnNode(DSUB),
+              MethodInsnNode(INVOKESTATIC, "java/lang/Math", "abs", "(D)D"),
+              LdcInsnNode(Double("3.0")),
+              InsnNode(DCMPL),
+              JumpInsnNode(IFLE, l0),
+              l3,
+              FrameNode(F_SAME, 0, null, 0, null),
+              FieldInsnNode(GETSTATIC, "sr", "d", "Lsr;"),
+              InsnNode(ARETURN),
+              l0,
+              FrameNode(F_SAME, 0, null, 0, null),
+              VarInsnNode(ALOAD, 0),
+              LdcInsnNode(Float("0.2")),
+              LdcInsnNode(Float("0.2")),
+              MethodInsnNode(INVOKEVIRTUAL, "sq", "a", "(FF)V"),
+              VarInsnNode(ALOAD, 0),
+              LdcInsnNode(Float("0.2")),
+              FieldInsnNode(PUTFIELD, "sq", "N", "F"),
+              VarInsnNode(ALOAD, 0),
+              FieldInsnNode(GETFIELD, "sq", "q", "Laab;"),
+              VarInsnNode(ILOAD, 1),
+              VarInsnNode(ILOAD, 2),
+              VarInsnNode(ILOAD, 3),
+              MethodInsnNode(INVOKEVIRTUAL, "aab", "f", "(III)Z"),
+              JumpInsnNode(IFEQ, l4),
+              VarInsnNode(ALOAD, 0),
+              FieldInsnNode(GETFIELD, "sq", "q", "Laab;"),
+              VarInsnNode(ILOAD, 1),
+              VarInsnNode(ILOAD, 2),
+              VarInsnNode(ILOAD, 3),
+              MethodInsnNode(INVOKEVIRTUAL, "aab", "h", "(III)I"),
+              VarInsnNode(ISTORE, 4),
+              VarInsnNode(ILOAD, 4),
+              MethodInsnNode(INVOKESTATIC, "ale", "j", "(I)I"),
+              VarInsnNode(ISTORE, 5),
+              LdcInsnNode(Float("0.5")),
+              VarInsnNode(FSTORE, 6),
+              LdcInsnNode(Float("0.5")),
+              VarInsnNode(FSTORE, 7),
+              VarInsnNode(ILOAD, 5),
+              TableSwitchInsnNode(0, 3, l9, [l5, l6, l7, l8]),
+              l5,
+              FrameNode(F_FULL, 8, ["sq", INTEGER, INTEGER, INTEGER, INTEGER, INTEGER, FLOAT, FLOAT], 0, []),
+              LdcInsnNode(Float("0.9")),
+              VarInsnNode(FSTORE, 7),
+              JumpInsnNode(GOTO, l9),
+              l6,
+              FrameNode(F_SAME, 0, null, 0, null),
+              LdcInsnNode(Float("0.1")),
+              VarInsnNode(FSTORE, 6),
+              JumpInsnNode(GOTO, l9),
+              l7,
+              FrameNode(F_SAME, 0, null, 0, null),
+              LdcInsnNode(Float("0.1")),
+              VarInsnNode(FSTORE, 7),
+              JumpInsnNode(GOTO, l9),
+              l8,
+              FrameNode(F_SAME, 0, null, 0, null),
+              LdcInsnNode(Float("0.9")),
+              VarInsnNode(FSTORE, 6),
+              l9,
+              FrameNode(F_SAME, 0, null, 0, null),
+              VarInsnNode(ALOAD, 0),
+              VarInsnNode(ILOAD, 5),
+              MethodInsnNode(INVOKESPECIAL, "sq", "x", "(I)V"),
+              VarInsnNode(ALOAD, 0),
+              VarInsnNode(ILOAD, 1),
+              InsnNode(I2F),
+              VarInsnNode(FLOAD, 6),
+              InsnNode(FADD),
+              InsnNode(F2D),
+              VarInsnNode(ILOAD, 2),
+              InsnNode(I2F),
+              LdcInsnNode(Float("0.9375")),
+              InsnNode(FADD),
+              InsnNode(F2D),
+              VarInsnNode(ILOAD, 3),
+              InsnNode(I2F),
+              VarInsnNode(FLOAD, 7),
+              InsnNode(FADD),
+              InsnNode(F2D),
+              MethodInsnNode(INVOKEVIRTUAL, "sq", "b", "(DDD)V"),
+              JumpInsnNode(GOTO, l10),
+              l4,
+              FrameNode(F_FULL, 4, ["sq", INTEGER, INTEGER, INTEGER], 0, []),
+              VarInsnNode(ALOAD, 0),
+              VarInsnNode(ILOAD, 1),
+              InsnNode(I2F),
+              LdcInsnNode(Float("0.5")),
+              InsnNode(FADD),
+              InsnNode(F2D),
+              VarInsnNode(ILOAD, 2),
+              InsnNode(I2F),
+              LdcInsnNode(Float("0.9375")),
+              InsnNode(FADD),
+              InsnNode(F2D),
+              VarInsnNode(ILOAD, 3),
+              InsnNode(I2F),
+              LdcInsnNode(Float("0.5")),
+              InsnNode(FADD),
+              InsnNode(F2D),
+              MethodInsnNode(INVOKEVIRTUAL, "sq", "b", "(DDD)V"),
+              l10,
+              FrameNode(F_SAME, 0, null, 0, null),
+              VarInsnNode(ALOAD, 0),
+              InsnNode(ICONST_1),
+              FieldInsnNode(PUTFIELD, "sq", "ca", "Z"),
+              VarInsnNode(ALOAD, 0),
+              InsnNode(ICONST_0),
+              FieldInsnNode(PUTFIELD, "sq", "b", "I"),
+              VarInsnNode(ALOAD, 0),
+              TypeInsnNode(NEW, "t"),
+              InsnNode(DUP),
+              VarInsnNode(ILOAD, 1),
+              VarInsnNode(ILOAD, 2),
+              VarInsnNode(ILOAD, 3),
+              MethodInsnNode(INVOKESPECIAL, "t", "<init>", "(III)V"),
+              FieldInsnNode(PUTFIELD, "sq", "cb", "Lt;"),
+              VarInsnNode(ALOAD, 0),
+              VarInsnNode(ALOAD, 0),
+              VarInsnNode(ALOAD, 0),
+              InsnNode(DCONST_0),
+              InsnNode(DUP2_X1),
+              FieldInsnNode(PUTFIELD, "sq", "y", "D"),
+              InsnNode(DUP2_X1),
+              FieldInsnNode(PUTFIELD, "sq", "z", "D"),
+              FieldInsnNode(PUTFIELD, "sq", "x", "D"),
+              FieldInsnNode(GETSTATIC, "sr", "a", "Lsr;"),
+              InsnNode(ARETURN),
+            ],
+            "\t* Returning player's ability to lie in bed in ",
+            INSERT_BEFORE
+          ).process(mn);
+        },
+        "ci()Z": function(mn)
+        {
+          check(mn, 0x1977043F);
+          CodeInserter(
+            BeginningFinder(),
+            [
+              InsnNode(ICONST_0),
+              InsnNode(IRETURN),
+            ],
+            "\t* But player shouldn't fall asleep in ",
+            INSERT_BEFORE
+          ).process(mn);
+        },
+        "l_()V": function(mn)
+        {
+          check(mn, 0x99096FE7);
+          log("\t* Player will want to nap at day in " + mn.name + mn.desc, 1);
+          var i;
+          for (i = 0; i < mn.instructions.size(); i++)
+          {
+            var n = mn.instructions.get(i);
+            if (isInstance(n, "org.objectweb.asm.tree.MethodInsnNode") && n.owner.equals("aab") && n.name.equals("v") && n.desc.equals("()Z"))
+            {
+              n = mn.instructions.get(i + 1);
+              if (isInstance(n, "org.objectweb.asm.tree.JumpInsnNode"))
+              {
+                mn.instructions.insertBefore(n, InsnNode(POP));
+                mn.instructions.set(n, JumpInsnNode(GOTO, n.label));
+                log("");
+                return;
+              }
+            }
+          }
+          log(" ...failed!");
+          recordFailure();
+        },
+        "a(ZZZ)V": function(mn)
+        {
+          check(mn, 0xE2142C81);
+          log("\t* Player will not set spawn chunk in " + mn.name + mn.desc, 1);
+          var i;
+          for (i = 0; i < mn.instructions.size(); i++)
+          {
+            var n = mn.instructions.get(i);
+            if (isInstance(n, "org.objectweb.asm.tree.VarInsnNode") && n.opcode == ILOAD && n["var"] == 3)
+            {
+              n = mn.instructions.get(i + 1);
+              if (isInstance(n, "org.objectweb.asm.tree.JumpInsnNode"))
+              {
+                mn.instructions.insertBefore(n, InsnNode(POP));
+                mn.instructions.set(n, JumpInsnNode(GOTO, n.label));
+                log("");
+                return;
+              }
+            }
+          }
+          log(" ...failed!");
+          recordFailure();
         }
       },
     },
@@ -2561,6 +2816,12 @@ function ObjectArray(arr)
     return new asm.tree.FrameNode(par1, par2, localvar, par4, stack);
   }
 
+  function TableSwitchInsnNode(par1, par2, par3, labels)
+  {
+    if (labels instanceof Array) labels = JavaArray(asm.tree.LabelNode, labels);
+    return new asm.tree.TableSwitchInsnNode(par1, par2, par3, labels);
+  }
+
   function LabelNode(label)
   {
     if (typeof label != "undefined")
@@ -2610,10 +2871,12 @@ function ObjectArray(arr)
     var count = 0;
     var total = length(a.tweakMethods);
     if (onClient()) total += length(a.tweakClientMethods);
+    var toRemove = [];
+    var mn;
 
     for (var i = 0; i < cn.methods.size(); i++)
     {
-      var mn = cn.methods.get(i);
+      mn = cn.methods.get(i);
       //log(mn.name);
       var tweakfunc;
       if (a.tweakMethods && (tweakfunc = a.tweakMethods[mn.name + mn.desc]))
@@ -2632,7 +2895,18 @@ function ObjectArray(arr)
           count++;
         }
       }
+      if (a.removeMethods && (a.removeMethods.indexOf(mn.name + mn.desc) != -1))
+      {
+        toRemove.push(mn);
+      }
     }
+    for (i = 0; i < toRemove.length; i++)
+    {
+      mn = toRemove[i];
+      log("Class " + cn.name + ": Removing " + mn.name + mn.desc);
+      cn.methods.remove(mn);
+    }
+
     if (a.add) a.add(cn);
     if (count < total)
     {
